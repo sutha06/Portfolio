62b3555f2738709394bf370e07e63caab806225a
sutha06 <suthakaran.siva0601@gmail.com>
2025-01-12 18:47:34 -0500

    nice navbar menu

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         </Link>
       </div>
 
-      {/* Hamburger Menu for Mobile */}
+      {/* Hamburger Menu */}
       <div className="menu-toggle" onClick={toggleMenu}>
         <div className={`hamburger ${isMenuOpen ? "open" : ""}`}>
           <span></span>
@@ -27,20 +27,50 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Links */}
-      <div className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
-        <Link to="/" className="hover-link" onClick={toggleMenu}>
-          Home
-        </Link>
-        <Link to="/portfolio" className="hover-link" onClick={toggleMenu}>
-          Portfolio
-        </Link>
-        <Link to="/experience" className="hover-link" onClick={toggleMenu}>
-          Experience
-        </Link>
-        <Link to="/contact" className="hover-link" onClick={toggleMenu}>
-          Contact
-        </Link>
+      {/* Full-Screen Overlay Menu */}
+      <div className={`navbar-overlay ${isMenuOpen ? "open" : ""}`}>
+        <ul className="navbar-links">
+          <li
+            style={{
+              transform: isMenuOpen ? "translateY(0)" : "translateY(120px)",
+              transitionDelay: isMenuOpen ? "0.8s" : "0s",
+            }}
+          >
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            style={{
+              transform: isMenuOpen ? "translateY(0)" : "translateY(120px)",
+              transitionDelay: isMenuOpen ? "0.9s" : "0s",
+            }}
+          >
+            <Link to="/portfolio" onClick={toggleMenu}>
+              Portfolio
+            </Link>
+          </li>
+          <li
+            style={{
+              transform: isMenuOpen ? "translateY(0)" : "translateY(120px)",
+              transitionDelay: isMenuOpen ? "1s" : "0s",
+            }}
+          >
+            <Link to="/experience" onClick={toggleMenu}>
+              Experience
+            </Link>
+          </li>
+          <li
+            style={{
+              transform: isMenuOpen ? "translateY(0)" : "translateY(120px)",
+              transitionDelay: isMenuOpen ? "1.1s" : "0s",
+            }}
+          >
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
